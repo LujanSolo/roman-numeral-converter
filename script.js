27d151e66c8de075a3c6526dc1e928f44f04e6f6
LujanSolo <lujansolo@proton.me>
2024-01-06 20:47:09 -0800

    added event listener to convert button and added conditionals to verify number input

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const convertBtn = document.getElementById("convert-btn");
 const userInput = document.getElementById("number");
 const output = document.getElementById("output");
+
+const hideToggle = () => {
+  output.classList.toggle("hidden");
+}
+convertBtn.addEventListener("click", () => {
+  const number = Number(userInput.value);
+
+  if(!number) {
+    output.textContent = "Please enter a valid number";
+    hideToggle();
+  } else if (number <= 0) {
+    output.textContent = "Please enter a number greater than or equal to 1";
+    hideToggle();
+  } else if (number >= 4000){
+    output.textContent = "Please enter a number less than or equal to 3999";
+    hideToggle();
+  }
+});
