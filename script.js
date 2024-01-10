c947f1c8643de8751b7491a2ff66911748112645
LujanSolo <lujansolo@proton.me>
2024-01-09 18:00:05 -0800

    added checkUserInput function to include validation and to later include the converter function

--- script.js
+++ script.js
@@ -6,26 +6,57 @@ const hideToggle = () => {
   output.classList.toggle("hidden");
 };
 
-const arabicToRoman = (number) => {
-  let romanSequence = "";
-}
+const arabicToRoman = (input) => {
 
-convertBtn.addEventListener("click", () => {
-  const number = Number(userInput.value);
+  const romanArray = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
 
-  if(!number) {
+};
+
+const checkUserInput = () => {
+  const inputNumberValue = parseInt(userInput.value);
+
+  if (!userInput.value || isNaN(inputNumberValue)) {
     output.textContent = "Please enter a valid number";
     hideToggle();
-  } else if (number <= 0) {
-    output.textContent = "Please enter a number greater than or equal to 1";
+    return;
+  } else if (inputNumberValue <= 0 || inputNumberValue >= 4000) {
+    output.textContent = "Please enter a number between 1 and 3999";
     hideToggle();
-  } else if (number >= 4000){
-    output.textContent = "Please enter a number less than or equal to 3999";
-    hideToggle();
+    return;
   };
 
 
-});
+  userInput.value = "";
+};
+
+
+// convertBtn.addEventListener("click", () => {
+//   const number = Number(userInput.value);
+
+//   if(!number) {
+    
+//   } else if (number <= 0) {
+//     output.textContent = "Please enter a number greater than or equal to 1";
+//     hideToggle();
+//   } else if (number >= 4000){
+//     output.textContent = "Please enter a number less than or equal to 3999";
+//     hideToggle();
+//   };
+// });
 
 // const numberInput = document.getElementById("number-input");
 // const convertBtn = document.getElementById("convert-btn");
